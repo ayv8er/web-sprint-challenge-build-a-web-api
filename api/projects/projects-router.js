7f6639f1f5b1ed7620091cbf174bc4cd2b810ef5
Richard Oh <n24512@protonmail.com>
2021-10-01 09:53:09 -0500

    server up and running

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,7 @@
 // Write your "projects" router here!
+const express = require("express");
+const {} = require("./projects-middleware");
+const Projects = require("./projects-model");
+const Actions = require("../actions/actions-model");
+
+const router = express.Router();
